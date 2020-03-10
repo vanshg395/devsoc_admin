88bf5b8ba7b22778657cd527070776096abfb050
Vansh Goel <vanshg395@gmail.com>
2020-03-11 03:34:30 +0530

    removed a dep

--- lib/board/assign_screen.dart
+++ lib/board/assign_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_multiselect/flutter_multiselect.dart';
 import 'package:http/http.dart' as http;
 
 import '../providers/auth.dart';
